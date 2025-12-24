9da20f8b0411ea976e32910faae7d4871d9f9a74
Christodoulos Tsoulloftas <chris@komposta.net>
2025-12-24 17:30:32 +0200

    Deploy to gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import { fileURLToPath } from "node:url";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/stridex/",
   server: {
     host: true,
     port: 5173,
