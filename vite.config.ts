e0f43f03119c066a7a5a22f7202bf8680f1c2d51
Christodoulos Tsoulloftas <chris@komposta.net>
2026-01-03 20:24:47 +0200

    Split js chunks

--- vite.config.ts
+++ vite.config.ts
@@ -15,4 +15,35 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom"],
+          mantine: [
+            "@mantine/core",
+            "@mantine/hooks",
+            "@mantine/dates",
+            "@mantine/form",
+            "@mantine/modals",
+          ],
+          dnd: ["@dnd-kit/core", "@dnd-kit/sortable", "@dnd-kit/utilities"],
+          google: [
+            "@react-oauth/google",
+            "@googleworkspace/drive-picker-react",
+            "@googleworkspace/drive-picker-element",
+          ],
+          i18n: [
+            "i18next",
+            "react-i18next",
+            "i18next-browser-languagedetector",
+            "i18next-http-backend",
+          ],
+          dayjs: ["dayjs"],
+          state: ["zustand", "zod", "mantine-form-zod-resolver"],
+          icons: ["@tabler/icons-react"],
+        },
+      },
+    },
+  },
 });
